18d37a8c4a9301ffb345c3d92f99d923622933de
Phi Hung <phihunds1@gmail.com>
2020-10-08 21:52:05 +0700

    buoc 6 hoan thien

--- thuchanhtoantu.js
+++ thuchanhtoantu.js
@@ -2,3 +2,5 @@ let inputWidth = prompt("Nhập chiều rộng")
 let inputHeight = prompt("Nhập chiều cao")
 let width = parseInt(inputWidth)
 let height = parseInt(inputHeight)
+let area = width * height
+document.write("Diện tích hình chữ nhật là:" + area)
