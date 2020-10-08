599fac6117b6c630b31ead8725777fd6d738f055
Phi Hung <phihunds1@gmail.com>
2020-10-08 21:52:45 +0700

    hoan thien

--- thuchanhtoantu.js
+++ thuchanhtoantu.js
@@ -3,4 +3,4 @@ let inputHeight = prompt("Nhập chiều cao")
 let width = parseInt(inputWidth)
 let height = parseInt(inputHeight)
 let area = width * height
-document.write("Diện tích hình chữ nhật là:" + area)
+document.write("Diện tích hình chữ nhật là: " + area)
